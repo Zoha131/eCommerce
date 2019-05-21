c6f91c2b9a4e1f29f801258de344f2a714195c46
zoha131 <abirhasanzoha@gmail.com>
2019-05-21 22:53:51 +0600

    added fragment

--- app/src/main/java/io/github/zoha131/ecommerce/MainActivity.kt
+++ app/src/main/java/io/github/zoha131/ecommerce/MainActivity.kt
@@ -2,11 +2,26 @@ package io.github.zoha131.ecommerce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import io.github.zoha131.ecommerce.core.appComponent
+import io.github.zoha131.ecommerce.databinding.ActivityMainBinding
+import io.github.zoha131.ecommerce.di.ActivityComponent
+import io.github.zoha131.ecommerce.shopping.productList.ProductListFragment
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var activityComponent: ActivityComponent
+    lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityComponent = appComponent.getActivityComponent().create(this)
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().replace(R.id.framelayout,
+            ProductListFragment()
+        ).commit()
+
     }
 }
